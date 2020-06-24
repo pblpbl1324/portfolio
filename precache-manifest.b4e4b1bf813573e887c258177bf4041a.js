ddc61bcfca990655de5c21395fb380aab7b04ce9
Bill Gan <billgan12345@gmail.com>
2020-06-24 09:40:54 -0400

    Updates

---
+++ precache-manifest.b4e4b1bf813573e887c258177bf4041a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74b1594904cd314974af316703d4df9e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4e8e9938678aaa8241e8",
+    "url": "/portfolio/static/css/main.c5b57c35.chunk.css"
+  },
+  {
+    "revision": "7bc6408bab9db6b10900",
+    "url": "/portfolio/static/js/2.564f0214.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.564f0214.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e8e9938678aaa8241e8",
+    "url": "/portfolio/static/js/main.05fcc9b7.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "e60290dc1682827098e030a02b5d796c",
+    "url": "/portfolio/static/media/font-bold.e60290dc.otf"
+  },
+  {
+    "revision": "7ef6d1bd7aaeb8d20f91e720898575e8",
+    "url": "/portfolio/static/media/font-light.7ef6d1bd.otf"
+  },
+  {
+    "revision": "380f993e84b88732505da21d3cdd4642",
+    "url": "/portfolio/static/media/github.380f993e.png"
+  },
+  {
+    "revision": "69a3626742c4b92dabea987a136d5363",
+    "url": "/portfolio/static/media/instagram.69a36267.png"
+  },
+  {
+    "revision": "f8be84e02357a2766aecb9c897b53826",
+    "url": "/portfolio/static/media/profile.f8be84e0.png"
+  },
+  {
+    "revision": "7ab3b1e13c59e9e5693a1af5e82b61e8",
+    "url": "/portfolio/static/media/project1.7ab3b1e1.png"
+  },
+  {
+    "revision": "14f6a7bbcbdf4b18685cb491e3210970",
+    "url": "/portfolio/static/media/project2.14f6a7bb.png"
+  },
+  {
+    "revision": "d821fe3d0ac0cf760f1c93175a307a82",
+    "url": "/portfolio/static/media/project3.d821fe3d.png"
+  },
+  {
+    "revision": "8f35a40403a84631c4125c4f1859c7a6",
+    "url": "/portfolio/static/media/twitter.8f35a404.png"
+  }
+]);
